409d6d78ef059896c370f65d24079af924e2eb16
Ephraim Fabian <ephraim.fabian@gmail.com>
2020-03-15 15:26:14 -0700

    Update Settings_IOS13.py
    Added Variables to the following classes:
    General (Keyboard, Fonts, Language and Region, Dictionary, VPN), Control_Center, Display_and_Brightness, Wallpaper, Siri and Search, Face ID and Passcode, Emergency SOS, Battery

--- Settings_IOS13.py
+++ Settings_IOS13.py
@@ -166,14 +166,146 @@ class Settings:
             Set_Automatically_Toggle = None
             Time_Zone = None
             Date_and_Time_Manual = None
+            
+        class Keyboard:
+            Keyboards_List = None
+            Text_Replacement_List = None
+            One_Handed_Keyboard = None
+            Auto_Capitalization_Toggle = None
+            Auto_Correction_Toggle = None
+            Enable_Caps_Lock_Toggle = None
+            Smart_Punctuation_Toggle = None
+            Character_Preview_Toggle = None
+            Dot_Shortcut_Toggle = None
+            Check_Spelling_Toggle = None
+            Predictive_Toggle = None
+            Slide_to_Type_Toggle = None
+            Delete_Slide_to_Type_by_Word_Toggle = None
+            Enable_Dictation_Toggle = None
+            Memoji_Stickers_Toggle = None
+            
+        class Fonts:
+            Fonts_List = None
+        
+        class Language_and_Region:
+            iPhone_Language = None
+            Region = None
+            Calendar = None
+            Temperature_Unit = None
+           
+        class Dictionary:
+            Dictionary_List = None
+            
+        class VPN:
+            Status_Toggle = None
+            VPN_List = None
+            
+        #class Legal_and_Regulatory:
+        #class Reset:
 
-#    class Control_Center:
-#    class Display_and_Brightness:
-#    class Wallpaper:
-#    class Siri_and_Search:
-#    class Touch_ID_and_Passcode:
-#    class Emergency_SOS:
-#    class Battery:
+    class Control_Center:
+        Access_Within_Apps_Toggle = None
+        Customize_Controls_List = None
+    
+    class Display_and_Brightness:
+        class Appearance:
+            Selection = None
+            Automatic_Toggle = None
+            Schedule = None
+            Light_Time = None
+            Dark_Time = None
+        
+        Brightness_Value = None
+        True_Tone_Toggle = None
+        
+        class Night_Shift:
+            Scheduled_Toggle = None
+            From_Time = None
+            To_Time = None
+            Manually_Enabled_Until_Tomorrow = None
+            Color_Temperature_Value = None
+
+        Auto_Lock = None
+        Raise_to_Wake_Toggle = None
+        Text_Size_Value = None
+        Bold_Text_Toggle = None
+        View_Selection = None
+    
+    #class Accessibility:
+    
+    class Wallpaper:
+        Lock_Screen = None
+        Home_Screen = None
+        Dark_Appearance_Dims_Wallpaper_Toggle = None
+            
+    class Siri_and_Search:
+        Listen_for_Hey_Siri_Toggle = None
+        Press_Side_Button_for_Siri_Toggle = None
+        Allow_Siri_When_Locked_Toggle = None
+        Language = None
+        Siri_Voice = None
+        Voice_Feedback = None
+        My_Information = None
+        Siri_and_Dictation_History = None
+        
+        #....Variables below are also from 'Notifications' class:
+        #class Announce_Messages:
+        #Announce_Messages_with_Siri_Toggle = None
+        #Announce_Messages_From_App = None
+        #Reply_without_Confirmation = None
+        
+        Suggestions_in_Search_Toggle = None
+        Suggestions_in_Look_Up_Toggle = None
+        Suggestions_on_Lock_Screen_Toggle = None
+        
+        class Third_Party_Apps:
+            #...Get List of Third_Party_Apps Function
+            Learn_from_this_App_Toggle = None
+            Show_in_Search_Toggle = None
+            Suggest_Shortcuts_Toggle = None
+            Show_Siri_Suggestions_Toggle = None
+        
+    class Face_ID_and_Passcode:
+        iPhone_Unlock_Toggle = None
+        iTunes_and_App_Store_Toggle = None
+        Apple_Pay_Toggle = None
+        Password_AutoFill_Toggle = None
+        
+        #class Third_Party_Apps
+            #...Get List of Third_Party_Apps Function
+            #App_Toggle = None
+        
+        Require_Attention_for_Face_ID_Toggle = None
+        Attention_Aware_Features_Toggle = None
+        Require_Passcode = None
+        Voice_Dial_Toggle = None
+        
+        Today_View_Toggle = None
+        Notification_Center_Toggle = None
+        Control_Center_Toggle = None
+        Siri_Toggle = None 
+        Replay_with_Message_Toggle = None
+        Home_Control_Toggle = None
+        Wallet_Toggle = None
+        Return_Missed_Calls_Toggle = None
+        USB_Accessories_Toggle = None
+        Erase_Data_Toggle = None
+
+    class Emergency_SOS:
+        Call_with_Side_Button_Toggle = None
+        Auto_Call_Toggle = None
+        Emergency_Contacts_List = None
+        Countdown_Sound_Toggle = None
+        
+    class Battery:
+        Low_Power_Mode_Toggle = None
+        
+        class Battery_Health:
+            Maximum_Capacity = None
+            Optimized_Battery_Charging_Toggle = None
+    
+    #.... Insert Battery Statistics Info....
+
 #    class Privacy:
 #    class iTunes_and_App_Store:
 #    class Wallet_and_Apple_Pay:
