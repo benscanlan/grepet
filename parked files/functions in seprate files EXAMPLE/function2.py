501f703007cc76ca22f52939ac339941d43c15be
Benjamin Scanlan <bscanlan@mail.csuchico.edu>
2020-05-04 05:57:26 -0700

    Initial commit
    Initial commit

--- parked files/functions in seprate files EXAMPLE/function2.py
+++
@@ -1,6 +0,0 @@
-from function1  import function1
-
-function1.myFun('Hello', 'Welcome', 'to', 'GeeksforGeeks')
-
-
-hello = hi
--- parked files/vars EXAMPLE/vardata.py
+++
@@ -1,3 +0,0 @@
-wverb_list = ["d", "y", "z"]
-other_list = [1,2, 3]
-something_else = False
