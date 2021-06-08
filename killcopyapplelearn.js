ebaf42f9c44087f21c8caf7a2af8691d300d7452
Benjamin Scanlan <bscanlan@mail.csuchico.edu>
2021-06-07 17:35:59 -0700

    disable copy paste w/ jquery

---
+++ killcopyapplelearn.js
@@ -0,0 +1,9 @@
+originalFrame.contents().bind('cut copy paste', function(event) {
+			e.preventDefault();
+
+jQuery is used by 73% of the 10 million most popular websites
+
+CDN https://www.w3schools.com/jquery/jquery_get_started.asp
+<head>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+</head>
