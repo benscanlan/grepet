2b23581ae41c4143b6d985026d5f8c8b535cc714
Benjamin Scanlan <bscanlan@mail.csuchico.edu>
2021-06-09 17:41:19 -0700

    check out grepet alina!

---
+++ headers.h
@@ -0,0 +1,5 @@
+#include <stdio.h> // Input Output for debugging
+#include <stdlib.h> //standard library <string.h> // strings, kinda important
+#include <errno.h>
+#include <sys/socket.h>
+#include <sys/types.h>
--- server.c
+++ server.c
@@ -5,11 +5,29 @@
 //             3
 //     respond
 
-#include <stdio.h> // Input Output for debugging
+#include "headers.h"
 int main() {
     // printf() displays the string inside quotation
      printf("Hello, World!");
     //returns a string
     //how about returns a filecontents
-     return 0;
+    FILE *filePointer ;
+    char dataToBeRead[50]; // can ask for size ahead of time of file for best memory ussage
+    filePointer = fopen("disable_context_menu.html", "r") ;
+    
+    //check if the file pointer is null if null route cannot be resolved send 404 page error code
+    if ( filePointer == NULL ) {
+          printf( "404 file failed to open." ) ;
+      }
+    else {
+        printf("The file is now opened.\n") ;
+            
+        while( fgets ( dataToBeRead, 50, filePointer ) != NULL ) { // using fgets() method fgets line by line
+              printf( "%s" , dataToBeRead ) ; // Print the dataToBeRead
+           }
+          fclose(filePointer) ;
+          printf("Data successfully read from file GfgTest.c\n");
+          printf("The file is now closed.") ;
+      }
+      return 0;
   }
