173948cb12be0f6a27228f8e1b16193749216edb
Benjamin Scanlan <bscanlan@mail.csuchico.edu>
2021-06-09 17:12:33 -0700

    starting server in scratch

--- cTops.c
+++ cTops.c
@@ -4,3 +4,10 @@
 //             2
 //             3
 //     respond
+
+#include <stdio.h> // Input Output for debugging
+int main() {
+    // printf() displays the string inside quotation
+     printf("Hello, World!");
+     return 0;
+  }
