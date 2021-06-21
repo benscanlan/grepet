b857e378fc29ee8629f1d9502b7e5411ea58703c
benscanlan <bscanlan@mail.csuchico.edu>
2021-06-21 15:56:43 -0700

    all

--- server.c
+++ server.c
@@ -79,6 +79,7 @@ int server() {
         int client_fd = accept(sockfd,(struct sockaddr *) &client_addr, &addr_size);
         if (client_fd > 0) {
             int n = read(client_fd, buffer, 2048);
+            printf("%d", n);
             printf("%s", buffer);
             fflush(stdout);
             n = write(client_fd, data, strlen(data));
