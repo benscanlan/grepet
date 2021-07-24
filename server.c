983a6e914ffb5b755b0098567933f3a0ddec46a2
benscanlan <bscanlan@mail.csuchico.edu>
2021-07-24 03:18:48 -0700

    highlight tab

--- server.c
+++ server.c
@@ -21,37 +21,37 @@
 //function writefile()
 
 int readfile() {
-// printf() displays the string inside quotation
-printf("enter a number like 1");
-char buf[10];
-fgets(buf,10,stdin);
-printf("%s", buf);
-//takes newline out of buffer read in https://stackoverflow.com/questions/2693776
-buf[strcspn(buf, "\n")] = 0;
-//int article = 1;
-//returns a filecontents
-FILE *filePointer ;
-char dataToBeRead[50]; // can ask for size ahead of time of file for best memory ussage
-char route[80];
-strcpy (route, "./articles/");
-strcat (route, buf);
-strcat (route, ".html");
-printf("%s", route);
-filePointer = fopen(route, "r") ;
+  // printf() displays the string inside quotation
+  printf("enter a number like 1");
+  char buf[10];
+  fgets(buf,10,stdin);
+  printf("%s", buf);
+  //takes newline out of buffer read in https://stackoverflow.com/questions/2693776
+  buf[strcspn(buf, "\n")] = 0;
+  //int article = 1;
+  //returns a filecontents
+  FILE *filePointer ;
+  char dataToBeRead[50]; // can ask for size ahead of time of file for best memory ussage
+  char route[80];
+  strcpy (route, "./articles/");
+  strcat (route, buf);
+  strcat (route, ".html");
+  printf("%s", route);
+  filePointer = fopen(route, "r") ;
 
-//check if the file pointer is null if null route cannot be resolved send 404 page error code
-if ( filePointer == NULL ) {
-  printf( "404 file failed to open." ) ;
-  }
-else {
-    //The file is now opened
-    while( fgets ( dataToBeRead, 50, filePointer ) != NULL ) { // using fgets() method fgets line by line
-       printf( "%s" , dataToBeRead ) ; // Print the dataToBeRead
-       }
-      fclose(filePointer) ;
-      //the file is now closed
-  }
-  return 0;
+  //check if the file pointer is null if null route cannot be resolved send 404 page error code
+  if ( filePointer == NULL ) {
+    printf( "404 file failed to open." ) ;
+    }
+  else {
+      //The file is now opened
+      while( fgets ( dataToBeRead, 50, filePointer ) != NULL ) { // using fgets() method fgets line by line
+         printf( "%s" , dataToBeRead ) ; // Print the dataToBeRead
+         }
+        fclose(filePointer) ;
+        //the file is now closed
+    }
+    return 0;
 }
 
 int server() {
@@ -65,9 +65,9 @@ int server() {
     // you cant run below 1024 as normal user. only as root can you run port 80 on ubuntu. bind is failing with no error message in logic. if port 80 run as sudo ./server on ubuntu
     //sudo ./server
     //sudo lsof -Pi tcp:80 | head -3
-    //sudo tcpdump port 80 // command works just slow to generate traffic data 
+    //sudo tcpdump port 80 // command works just slow to generate traffic data
 
-    
+
     struct sockaddr_storage client_addr;
     socklen_t addr_size = sizeof client_addr;
     char headers[] = "HTTP/1.0 200 OK\r\nServer: CPi\r\nContent-type:text/html\r\n\r\n";
@@ -79,7 +79,7 @@ int server() {
     int sockfd = socket(server->ai_family, server->ai_socktype, server->ai_protocol);
     bind(sockfd, server->ai_addr, server->ai_addrlen);
     listen(sockfd, 10);
-    
+
     while(1) {
         int client_fd = accept(sockfd,(struct sockaddr *) &client_addr, &addr_size);
         if (client_fd > 0) {
