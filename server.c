cbbc9de592156fdc356c44031ffdda6e0f49b40a
Benjamin Scanlan <bscanlan@mail.csuchico.edu>
2021-08-19 17:02:27 -0700

    we need system select

--- server.c
+++ server.c
@@ -1,10 +1,13 @@
 //https://hea-www.harvard.edu/~fine/Tech/addrinuse.html
+// goal build functionality like service httpd graceful restart not loose conections
+//A graceful restart tells the web sever to finish any active connections before restarting thats all tho
 //input output
 #include <stdio.h>
 #include <stdlib.h>
 //data types
 #include <string.h>
 //server()
+#include <sys/select.h>
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
@@ -31,7 +34,7 @@ int server() {
     memset(&hints, 0, sizeof hints);
     hints.ai_family =  AF_INET;
     hints.ai_socktype = SOCK_STREAM;
-    hints.ai_flags = SOCK_NONBLOCK || SO_REUSEADDR; //||
+    hints.ai_flags = SO_LINGER; //SOCK_NONBLOCK || SO_REUSEADDR; //|| //unused
 
     char* grepetcom = NULL; //www.grepet.com
     getaddrinfo(grepetcom, "80", &hints, &server); //run as sudo 80
@@ -42,7 +45,14 @@ int server() {
     char data[2048] = {0};
     snprintf(data, sizeof data,"%s %s", headers, html());
     int sockfd = socket(server->ai_family, server->ai_socktype, server->ai_protocol);
-    setsockopt(sockfd,SOCK_NONBLOCK ,SO_REUSEADDR,0,sizeof(int));//1
+    //setsockopt(sockfd,SOCK_NONBLOCK ,SO_REUSEADDR,0,sizeof(int));//1
+    //setsockopt(sockfd,SOCK_NONBLOCK ,SO_LINGER,0,sizeof(int));//1
+    //int setsockopt(int socket, int level, int option_name,const void *option_value, socklen_t option_len);
+    struct linger sl;
+    sl.l_onoff=1;
+    sl.l_linger=0;
+    setsockopt(sockfd,SOL_SOCKET, SO_LINGER,&sl,sizeof(sl));
+
     int g = bind(sockfd, server->ai_addr, server->ai_addrlen);
     printf("%d\n",g);
     if (g < 0) { return 0; }
@@ -51,11 +61,14 @@ int server() {
     read(client_fd, buffer, 2048);
     fflush(stdout);
     send(client_fd, data, strlen(data),0);
-    shutdown(sockfd, SHUT_WR);
+    fflush(stdout);
+    //shutdown(sockfd, SHUT_WR);
+    close(client_fd);
     close(sockfd);
 
 
-    close(client_fd);
+    //close(client_fd);
+    //shutdown(1,1);
     return 0;
 
 
