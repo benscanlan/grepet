af913f0e7d0e18bf42e4364d025a3b32233f1915
benscanlan <bscanlan@mail.csuchico.edu>
2022-01-06 18:50:20 -0800

    06:50:20 PM

--- push.sh
+++ push.sh
@@ -3,7 +3,7 @@ cd /home/ben/Documents/Github/grepet
 #git add -A
 git add .
 git commit -am "$(date +"%r")"
-git push
 SOMEVAR='benscanlan'
+git push
 echo "$SOMEVAR"
 echo "$(cat /home/ben/Documents/Github/tokenizer)"
